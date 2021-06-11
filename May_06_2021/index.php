db7f4f2bdd3135057c85209e72021ca698839cd0
Masum <masum2mina@gmai.com>
2021-06-11 07:38:47 +0600

    My first php basic class start again

---
+++ May_06_2021/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+	<div class="main_container">
+		<div class="header">
+			<h2>PHP Basic Start</h2>
+			<p>11 May 2021</p>
+		</div>
+		<div class="container">
+			<div class="inner_content">
+				
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<style type="text/css">
+	body{
+		background: #ddd;
+	}
+	.main_container {
+		width: 1024px;
+		margin: 0 auto;
+		background: #fff;
+
+	}
+	.header {
+		text-align: center;
+		margin: 0;
+		padding: 30px 0;
+		background: #444ee4;
+	}
+	.header h2 {
+		color: #fff;
+		margin: 0;
+	}
+	.header p {
+		
+		margin: 0;
+	}
+	.container {
+		background: #333;
+		padding: 10px;
+		
+	}
+	.inner_content {
+		background: #fff;
+		min-height: 600px;
+		width: 100%;
+
+
+	}
+
+</style>
+
+
+<?php 
+
+echo "May Allah help me to be a great programmer";
+
+?>
